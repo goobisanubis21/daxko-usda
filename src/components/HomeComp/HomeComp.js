11681ec08a710368a684f03dce4bcee7c67624f6
Anthony Parrino <anthony21parrino21@gmail.com>
2021-12-13 20:17:16 -0500

    created api call

--- src/components/HomeComp/HomeComp.js
+++ src/components/HomeComp/HomeComp.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import { React } from 'react'
 
 const HomeComp = () => {
 
-const USDA_URL = "https://api.nal.usda.gov/fdc/v1/foods/search?query=apple&pageSize=2&api_key=rTCM1DqeIDLeCShyqxcRK6OJWssuxxKlTl0FylLQ";
-const API_KEY = 'rTCM1DqeIDLeCShyqxcRK6OJWssuxxKlTl0FylLQ'
+    // const USDA_URL = "https://api.nal.usda.gov/fdc/v1/foods/search?query=apple&pageSize=2&api_key=rTCM1DqeIDLeCShyqxcRK6OJWssuxxKlTl0FylLQ";
+    // const API_KEY = 'rTCM1DqeIDLeCShyqxcRK6OJWssuxxKlTl0FylLQ'
+
+    function handleSearch() {
+        let search = document.querySelector('.search').value
+        fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?query=${search}&pageSize=2&api_key=rTCM1DqeIDLeCShyqxcRK6OJWssuxxKlTl0FylLQ`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+    }
 
     return (
         <div>
-            home page
+            <div>
+                <input className='search' placeholder='Enter a Food'></input>
+                <button onClick={handleSearch}>Search</button>
+            </div>
         </div>
     )
 }
