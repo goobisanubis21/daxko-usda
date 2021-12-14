a9c3cc1ed78d8c49281b77bb71ed37cc353f9cb4
Anthony Parrino <anthony21parrino21@gmail.com>
2021-12-13 19:05:52 -0500

    created files

---
+++ src/components/HomeComp/HomeComp.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HomeComp = () => {
+
+const USDA_URL = "https://api.nal.usda.gov/fdc/v1/foods/search?query=apple&pageSize=2&api_key=rTCM1DqeIDLeCShyqxcRK6OJWssuxxKlTl0FylLQ";
+const API_KEY = 'rTCM1DqeIDLeCShyqxcRK6OJWssuxxKlTl0FylLQ'
+
+    return (
+        <div>
+            home page
+        </div>
+    )
+}
+
+export default HomeComp
